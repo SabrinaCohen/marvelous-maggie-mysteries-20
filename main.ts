26cd35d88505247742fc2e9cf00baf0311655251
SabrinaCohen <82903380+SabrinaCohen@users.noreply.github.com>
2021-05-20 14:27:42 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 namespace SpriteKind {
     export const Currency = SpriteKind.create()
     export const LUCY = SpriteKind.create()
+    export const ANYA = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     game.over(false)
@@ -37,15 +38,54 @@ function startNextLevel () {
     } else if (currentLevel == 5) {
         scene.setBackgroundColor(10)
         tiles.setTilemap(tilemap`level5`)
+    } else if (false) {
+        scene.setBackgroundColor(2)
+        tiles.setTilemap(tilemap`level8`)
     } else {
         game.over(true)
     }
     tiles.placeOnRandomTile(Maggie, assets.tile`myTile3`)
     tiles.placeOnRandomTile(Lucy, assets.tile`myTile3`)
     for (let value2 of tiles.getTilesByType(assets.tile`myTile4`)) {
-        MyEnemy = sprites.create(assets.tile`myTile5`, SpriteKind.Enemy)
+        MyEnemy = sprites.create(assets.tile`myTile5`, SpriteKind.ANYA)
         tiles.placeOnTile(MyEnemy, value2)
         MyEnemy.follow(Maggie, 30)
+    }
+    for (let value3 of tiles.getTilesByType(assets.tile`myTile23`)) {
+        ANYA = sprites.create(img`
+            ................
+            ..111111111111..
+            .11ffffffffff11.
+            11ffff7777ffff11
+            1ffff777777ffff1
+            1f777f1777f177f1
+            1f777ff777ff77f1
+            1f777777477777f1
+            1f727777777727f1
+            1f772777777277f1
+            1ff77222222777f1
+            1ff7772222777ff1
+            1fff77777777fff1
+            1ffff777777ffff1
+            11112acccca21111
+            ...12a2a2a221...
+            ...12aaaaaa21...
+            ...12a2a2a221...
+            ...1caaaaaac1...
+            ...1ca2a2a2c1...
+            ...1caaaaaac1...
+            ...1ca2a2a2c1...
+            ...17aaaaaa71...
+            ...11a2a2a211...
+            ...11aaaaaa11...
+            ...1cccccccc1...
+            ...1c2c2c2c21...
+            ..11cccccccc11..
+            ..1c2c2c2c2c21..
+            ..1cccccccccc1..
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(ANYA, assets.tile`myTile23`)
+        ANYA.follow(Maggie, 20)
     }
 }
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
@@ -62,6 +102,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     game.over(false)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ANYA, function (sprite, otherSprite) {
+    info.changeLifeBy(-3)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     if (sprite.bottom == otherSprite.y) {
@@ -70,6 +113,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         info.changeLifeBy(-1)
     }
 })
+let ANYA: Sprite = null
 let MyEnemy: Sprite = null
 let currentLevel = 0
 let Lucy: Sprite = null
